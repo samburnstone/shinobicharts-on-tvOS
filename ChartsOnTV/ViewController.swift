6fb81c07e78282fb6defbbb630d874e30e88da0c
Sam Burnstone <sburnstone@scottlogic.com>
2016-04-01 10:41:19 +0100

    Adds base project files

---
+++ ChartsOnTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChartsOnTV
+//
+//  Created by Samuel Burnstone on 01/04/2016.
+//  Copyright © 2016 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
